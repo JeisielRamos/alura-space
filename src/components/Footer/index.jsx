c34bb4119d090b575e7337a24ae573b832cf4e64
jeisiel.oliveira <jeisielramos@gmail.com>
2023-06-26 19:56:40 -0300

    add fonts and footer and variables scss

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import styles from "./footer.module.scss"
+import gitlabLogo from "../../assets/icons/gitlab-logo.png";
+import gitHubLogo from "../../assets/icons/github-logo.png";
+import linkedinLogo from "../../assets/icons/linkedin-logo.png";
+
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footer__icons}>
+        <a
+          href="https://gitlab.com/JeisielRamos"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={gitlabLogo} alt="ícone do gitlab"  height="30"/>
+        </a>
+        <a
+          href="https://github.com/JeisielRamos"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={gitHubLogo} alt="ícone do github" height="30" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/jeisiel-ramos-a76953108/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={linkedinLogo} alt="ícone do linkedin" height="30"/>
+        </a>
+      </div>
+      <p>Desenvolvido por JeisielRamos@2023.</p>
+    </footer>
+  );
+}
